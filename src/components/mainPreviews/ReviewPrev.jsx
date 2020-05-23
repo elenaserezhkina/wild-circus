a740d8fdbf0402acf11f4e4b0ed76b7da9ac568e
Elena <anel92r@gmail.com>
2020-05-23 18:20:23 +0200

    add button more reviews

--- src/components/mainPreviews/ReviewPrev.jsx
+++ src/components/mainPreviews/ReviewPrev.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import _Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
@@ -7,6 +8,7 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import styled from "styled-components";
 import Color from "../../utils/Colors";
+import { ButtonsContainer, NavigationButton } from "../../pages/style";
 
 const Container = styled.div`
   margin: 0 auto;
@@ -25,7 +27,7 @@ const Card = styled(_Card)`
   margin: 10px;
   width: 100%;
   &.MuiPaper-root {
-    background-color: ${Color.mainYellow};
+    // background-color: ${Color.mainYellow};
     border-radius: 40px;
     color: ${Color.mainDark};
   }
@@ -49,6 +51,12 @@ const ReviewPrev = (props) => {
             </CardActionArea>
           ))}
       </Card>
+
+      <ButtonsContainer>
+        <Link to="/review">
+          <NavigationButton>more reviews</NavigationButton>
+        </Link>
+      </ButtonsContainer>
     </Container>
   );
 };
